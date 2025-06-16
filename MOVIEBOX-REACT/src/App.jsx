f026ed2aa1583bb65ee7781a08f4113dca8beb93
Muhammad Bilal Raza <bilalrazaswe@gmail.com>
2025-06-16 23:54:36 +0500

    📦 NEW: feat: refactor App component to include Navbar and improve layout structure

--- MOVIEBOX-REACT/src/App.jsx
+++ MOVIEBOX-REACT/src/App.jsx
@@ -1,18 +1,22 @@
-import './App.css'
+import "./App.css";
 import Home from "./pages/Home";
-import { Route, Routes } from 'react-router-dom';
-import Favorites from "./pages/Favorites"
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import Navbar from "./components/navabar";
 
+function App() {
   return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/favorites' element={<Favorites/>} />
-      </Routes>
-    </main>
-  )
+    <div>
+      <Navbar/>
+      <main className="main-routing">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ MOVIEBOX-REACT/src/components/navabar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <>
+      <div className="default-page-nav">
+        <Link to="/">Movie Box</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/home" className="nav-link">Home</Link>
+        <Link to="/favorites" className="nav-link">Favorites</Link>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
