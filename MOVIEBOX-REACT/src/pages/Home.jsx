4b16cfb49f0842b12aff55a9ceae357256462314
Muhammad Bilal Raza <bilalrazaswe@gmail.com>
2025-06-18 23:44:28 +0500

    feat: implement movie fetching functionality in Home component and add API service

--- MOVIEBOX-REACT/src/pages/Home.jsx
+++ MOVIEBOX-REACT/src/pages/Home.jsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "../components/movie-card";
-import "../css/home.css"
+import "../css/home.css";
+import { getPopularMovies, searchMovies } from "../services/api.js";
+
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [error, setError] = useState(false);
 
-  const movies = [
-    { id: 1, title: "The Shawshank Redemption", releaseDate: 1994 },
-    { id: 2, title: "The Godfather", releaseDate: 1972 },
-    { id: 3, title: "The Dark Knight", releaseDate: 2008 },
-    { id: 4, title: "12 Angry Men", releaseDate: 1957 },
-    { id: 5, title: "Schindler's List", releaseDate: 1997 },
-  ];
+  useEffect(() => {
+    const laodPopularMovies = async () => {
+      try {
+        setLoader(true);
+        setError(null);
+        const response = await getPopularMovies();
+        const mappedMovies = response.map((movie) =>({
+          id: movie.id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          description: movie.overview
+        }))
+        setMovies(mappedMovies);
+      } catch (error) {
+        console.log(error);
+        setError("Failed to laod Movies...");
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    laodPopularMovies();
+  }, []);
 
   function handleSearch(e) {
     e.preventDefault();
@@ -33,6 +55,14 @@ function Home() {
         </form>
       </div>
 
+      {error && (
+        <div
+          className="error-message"
+          style={{ color: "red", textAlign: "center", margin: "1em 0" }}
+        >
+          {error}
+        </div>
+      )}
       <div className="movies-grid">
         {movies.map(
           (movie) =>
