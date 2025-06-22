24dab8d2b2cd41c65d94be14f45d52743edb892a
Muhammad Bilal Raza <bilalrazaswe@gmail.com>
2025-06-22 21:07:15 +0500

    📦 NEW: add @vercel/speed-insights dependency and integrate SpeedInsights component in main application

--- MOVIEBOX-REACT/src/main.jsx
+++ MOVIEBOX-REACT/src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -7,6 +8,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>
 );
