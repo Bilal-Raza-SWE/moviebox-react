d7303d4252d425bc265a47eb8492767c23533a20
Muhammad Bilal Raza <bilalrazaswe@gmail.com>
2025-06-22 18:53:23 +0500

    🐛 FIX: refactor Navbar component for improved layout and styling, adding active link highlighting and responsive design

--- MOVIEBOX-REACT/src/components/navabar.jsx
+++ MOVIEBOX-REACT/src/components/navabar.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
-import "../css/navbar.css"
+import { Link, useLocation } from "react-router-dom";
+import "../css/navbar.css";
+
 function Navbar() {
+  const location = useLocation();
+
   return (
-    <>
-      <div className="default-page-nav">
-        <Link to="/">Movie Box</Link>
+    <div className="navbar-container">
+      <div className="navbar-content">
+        <div className="brand">
+          <Link to="/" className="brand-link">
+            <span className="brand-icon">🎬</span>
+            Movie Box
+          </Link>
+        </div>
+        <nav className="navbar-links">
+          <Link 
+            to="/home" 
+            className={`nav-link ${location.pathname === '/home' || location.pathname === '/' ? 'active' : ''}`}
+          >
+            Home
+          </Link>
+          <Link 
+            to="/favorites" 
+            className={`nav-link ${location.pathname === '/favorites' ? 'active' : ''}`}
+          >
+            Favorites
+          </Link>
+        </nav>
       </div>
-      <div className="navbar-links">
-        <Link to="/home" className="nav-link">Home</Link>
-        <Link to="/favorites" className="nav-link">Favorites</Link>
-      </div>
-    </>
+    </div>
   );
 }
 
